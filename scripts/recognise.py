118ca131bc4aa96e5349791ab4b2948fada218db
Joshua B <joshuab.development@gmail.com>
2023-04-14 16:57:24 +1000

    Object recognition is working

---
+++ scripts/recognise.py
@@ -0,0 +1,26 @@
+import rospy
+from find_object_2d.msg import ObjectsStamped
+
+def callback(msg):
+    # Iterate through the objects in the message
+    highest_confidence = 0
+    highest_id = -1
+    for i in range(0, len(msg.objects.data), 12):
+        object_id = int(msg.objects.data[i])
+        confidence = msg.objects.data[i + 1]
+        print("Recognized object ID:", object_id, "Confidence:", confidence)
+        if confidence > highest_confidence:
+            highest_confidence = confidence
+            highest_id = object_id
+    print("Highest confidence object ID:", highest_id, " Confidence:", highest_confidence)
+
+
+
+def main():
+    print("IM RUNNING FUCKER")
+    rospy.init_node('object_recognition_listener', anonymous=True)
+    rospy.Subscriber('/objectsStamped', ObjectsStamped, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
