b6de905be9b9a500ca129a620c59d868f3a8c0db
Mitchell Gust <s3782095@student.rmit.edu.au>
2023-04-15 20:58:45 +1000

    Sign Dictionary + Print Detected

--- scripts/recognise.py
+++ scripts/recognise.py
@@ -1,10 +1,30 @@
 import rospy
 from find_object_2d.msg import ObjectsStamped
 
-def callback(msg):
+signage_id = {
+    # 'Unknown' Included in the Assignment Specs - Potential New Sign shown on Day?
+    0: 'Unknown',
+    1: 'Explosive',
+    2: 'Flammable Gas',
+    3: 'Non-Flammable Gas',
+    4: 'Dangerous When Wet',
+    5: 'Flammable Solid',
+    6: 'Spontaneously Combustible',
+    7: 'Oxidizer',
+    8: 'Organic Peroxide',
+    9: 'Inhalation Hazard',
+    10: 'Poison',
+    11: 'Radioactive',
+    12: 'Corrosive',
+    13: 'Start'
+}
+
+def callback(msg):    
     # Iterate through the objects in the message
     highest_confidence = 0
     highest_id = -1
+    confidence = 0
+
     for i in range(0, len(msg.objects.data), 12):
         object_id = int(msg.objects.data[i])
         confidence = msg.objects.data[i + 1]
@@ -13,10 +33,14 @@ def callback(msg):
             highest_confidence = confidence
             highest_id = object_id
     print("Highest confidence object ID:", highest_id, " Confidence:", highest_confidence)
-    if highest_id == 10:
-        print("START SIGN FOUND")
 
+    # Considers If No Sign is Found
+    if highest_id != -1:
+        # Print the Sign Name found to have the Highest Confidence
+        print(signage_id[highest_id])
 
+    # Adds delay to the program
+    # rospy.sleep(2)    
 
 def main():
     print("IM RUNNING FUCKER")
