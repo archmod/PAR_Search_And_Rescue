e177371936953c1a5fcfa0835bbccb09152eaab8
Joshua B <joshuab.development@gmail.com>
2023-04-16 12:10:20 +1000

    Renamed the images to their correct ID's

--- scripts/recognise_node.py
+++ scripts/recognise_node.py
@@ -5,6 +5,7 @@ from std_msgs.msg import Int32
 # from par_search_and_rescue.msg import HazardMarker
 from geometry_msgs.msg import PoseStamped
 from enum import Enum
+from visualization_msgs.msg import Marker
 
 class Recognition_Image(Enum):
     UNKNOWN = 0
@@ -22,6 +23,8 @@ class Recognition_Image(Enum):
     CORROSIVE = 12
     START = 13
 
+
+
 class HazardDetection:
     def __init__(self):
         print("IM RUNNING BOSS")
@@ -29,8 +32,37 @@ class HazardDetection:
         rospy.Subscriber('/objectsStamped', ObjectsStamped, self.callback)
         # self.hazard_detected_pub = rospy.Publisher('/hazard_markers_detected', HazardMarker, queue_size=1)
         self.hazard_detected_pub = rospy.Publisher('/hazard_markers_detected', Int32, queue_size=1)
-
+        self.hazard_marker_pub = rospy.Publisher('/hazards', Marker, queue_size=10)
         rospy.spin()
+
+    def create_hazard_marker(self, marker_id, x, y, z):
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.type = marker.CUBE
+        marker.action = marker.ADD
+
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.position.z = z
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
+
+        marker.scale.x = 0.1
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+
+        marker.color.r = 1.0
+        marker.color.g = 0.0
+        marker.color.b = 0.0
+        marker.color.a = 1.0
+
+        marker.lifetime = rospy.Duration()
+
+        marker.id = marker_id
+
+        return marker
 
     def callback(self, msg):
         # Iterate through the objects in the message
@@ -49,6 +81,12 @@ class HazardDetection:
                 hazard_marker_id.data = object_id
                 self.hazard_detected_pub.publish(hazard_marker_id)
                 print(f"Published hazard marker {recognized_image.name} (ID: {object_id}) at (x, y): {x}, {y}")
+                hazMarker = self.create_hazard_marker(object_id, x, y, 0.0)
+                self.hazard_marker_pub.publish(hazMarker)
+                print(f"Published hazard marker {recognized_image.name} (ID: {object_id}) at (x, y): {x}, {y}")
+                # rostopic echo /hazard_markers_detected
+                # rostopic echo /hazards
+                
 
 
 if __name__ == '__main__':
