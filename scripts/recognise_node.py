f59bfed7eb29f7036f0e007b052ac37d33b02c59
Joshua B <joshuab.development@gmail.com>
2023-04-16 12:53:11 +1000

    fixed the images again....

--- scripts/recognise_node.py
+++ scripts/recognise_node.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import PoseStamped
 from enum import Enum
 from visualization_msgs.msg import Marker
 
+
 class Recognition_Image(Enum):
     UNKNOWN = 0
     EXPLOSIVE = 1
@@ -23,15 +24,12 @@ class Recognition_Image(Enum):
     CORROSIVE = 12
     START = 13
 
-
-
 class HazardDetection:
     def __init__(self):
         print("IM RUNNING BOSS")
         rospy.init_node('hazard_detection_node', anonymous=True)
         rospy.Subscriber('/objectsStamped', ObjectsStamped, self.callback)
         # self.hazard_detected_pub = rospy.Publisher('/hazard_markers_detected', HazardMarker, queue_size=1)
-        self.hazard_detected_pub = rospy.Publisher('/hazard_markers_detected', Int32, queue_size=1)
         self.hazard_marker_pub = rospy.Publisher('/hazards', Marker, queue_size=10)
         rospy.spin()
 
@@ -69,8 +67,8 @@ class HazardDetection:
         for i in range(0, len(msg.objects.data), 12):
             object_id = int(msg.objects.data[i])
             confidence = msg.objects.data[i + 1]
-            x = msg.objects.data[i + 6]
-            y = msg.objects.data[i + 7]
+            y = msg.objects.data[i + 6]
+            x = msg.objects.data[i + 7]
             
             recognized_image = Recognition_Image(object_id)
             print(f"Recognized {recognized_image.name} (ID: {object_id}) with Confidence: {confidence}")
@@ -79,8 +77,6 @@ class HazardDetection:
             if confidence > 0.5 and object_id != Recognition_Image.START.value:
                 hazard_marker_id = Int32()
                 hazard_marker_id.data = object_id
-                self.hazard_detected_pub.publish(hazard_marker_id)
-                print(f"Published hazard marker {recognized_image.name} (ID: {object_id}) at (x, y): {x}, {y}")
                 hazMarker = self.create_hazard_marker(object_id, x, y, 0.0)
                 self.hazard_marker_pub.publish(hazMarker)
                 print(f"Published hazard marker {recognized_image.name} (ID: {object_id}) at (x, y): {x}, {y}")
